3b0768e3d380fb64774ffceb4ba80964e21d5ed1
Dakota Erickson <derickson29@gmail.com>
2026-02-22 15:14:50 -0600

    adjust auction start/end times

--- app/config.py
+++ app/config.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AUCTION_END: datetime = datetime.fromisoformat(
-    os.getenv("AUCTION_END_TIME", "2099-01-01T00:00:00")
+    os.getenv("AUCTION_END_TIME", "2026-04-17T20:59:59")
+)
+
+AUCTION_START: datetime = datetime.fromisoformat(
+    os.getenv("AUCTION_START_TIME", "2026-04-10T11:59:59")
 )
 
 def auction_is_open() -> bool:
-    return datetime.now() < AUCTION_END
+    return AUCTION_START < datetime.now() < AUCTION_END
