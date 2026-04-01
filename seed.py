a526b2bc65b7708204f1b718621ad2d462fe4783
Dakota Erickson <derickson29@gmail.com>
2026-04-01 16:06:27 -0500

    organize auction items and standardize wording in descriptions

--- seed.py
+++ seed.py
@@ -8,26 +8,6 @@ from app import models
 
 ITEMS = [
     {
-        "name": "Front Row Seats Music Program: 3rd Grade",
-        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 3rd Grade",
-        "starting_bid": 50.00,
-    },
-    {
-        "name": "Front Row Seats Music Program: 3rd Grade",
-        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 3rd Grade",
-        "starting_bid": 50.00,
-    },
-    {
-        "name": "Front Row Seats Music Program: 4th Grade",
-        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 4th Grade",
-        "starting_bid": 50.00,
-    },
-    {
-        "name": "Front Row Seats Music Program: 4th Grade",
-        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 4th Grade",
-        "starting_bid": 50.00,
-    },
-    {
         "name": "Front Row Seats Music Program: 1st Grade",
         "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Spring 2027 - 1st Grade",
         "starting_bid": 50.00,
@@ -45,6 +25,26 @@ ITEMS = [
     {
         "name": "Front Row Seats Music Program: 2nd Grade",
         "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Spring 2027 - 2nd Grade",
+        "starting_bid": 50.00,
+    },
+    {
+        "name": "Front Row Seats Music Program: 3rd Grade",
+        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 3rd Grade",
+        "starting_bid": 50.00,
+    },
+    {
+        "name": "Front Row Seats Music Program: 3rd Grade",
+        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 3rd Grade",
+        "starting_bid": 50.00,
+    },
+    {
+        "name": "Front Row Seats Music Program: 4th Grade",
+        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 4th Grade",
+        "starting_bid": 50.00,
+    },
+    {
+        "name": "Front Row Seats Music Program: 4th Grade",
+        "description": "Up to five (5) reserved seats in the front row at MPAAC for the Music Program Fall 2026 - 4th Grade",
         "starting_bid": 50.00,
     },
     {
@@ -59,11 +59,6 @@ ITEMS = [
         "starting_bid": 50.00,
     },
     {
-        "name": "Foxy Treat at Maize City Park",
-        "description": "The student's entire class will earn a trip to the new Foxi Ice Cream to enjoy an ice cream at the Maize City Park",
-        "starting_bid": 50.00, # VERIFY THIS STARTING PRICE
-    },
-    {
         "name": "Parent Pick Up Reserved Spot: Quarter 3 2027",
         "description": "January 6th to March 12th",
         "starting_bid": 50.00,
@@ -74,59 +69,59 @@ ITEMS = [
         "starting_bid": 50.00,
     },
     {
+        "name": "Foxi Treat at Maize City Park",
+        "description": "Your student's entire class will earn a trip to the new Foxi Ice Cream to enjoy an ice cream at the Maize City Park",
+        "starting_bid": 50.00,
+    },
+    {
         "name": "Principal For A Day",
         "description": "Your student will accompany Ms. Dow throughout the day and enjoy \"Principal Duties\"",
         "starting_bid": 50.00,
     },
     {
-        "name": "Class Pizza Party Bus Ride",
-        "description": "The student's entire class will earn a special ride on a party bus including pizza to enjoy during the ride",
+        "name": "Pizza Party",
+        "description": "Your student's entire class will enjoy pizza and games at Maize City Park",
         "starting_bid": 50.00,
     },
     {
-        "name": "Cupcake Party #1",
-        "description": "Entire class will receive a cupcake party",
+        "name": "Sonic Drink Party",
+        "description": "Your student's entire class will earn Sonic drinks delivered to them at school",
         "starting_bid": 20.00,
     },
     {
-        "name": "Donut Party #1",
-        "description": "Entire class will receive a donut party",
+        "name": "Lunch with Ms. Dow",
+        "description": "Your student will go out to lunch with Ms. Dow!",
+        "starting_bid": 20.00,
+    },
+    {
+        "name": "Cupcake Party #1",
+        "description": "Your student's entire class will receive a cupcake party",
         "starting_bid": 20.00,
     },
     {
         "name": "Cupcake Party #2",
-        "description": "Entire class will receive a cupcake party",
-        "starting_bid": 20.00,
-    },
-    {
-        "name": "Donut Party #2",
-        "description": "Entire class will receive a donut party",
+        "description": "Your student's entire class will receive a cupcake party",
         "starting_bid": 20.00,
     },
     {
         "name": "Cupcake Party #3",
-        "description": "Entire class will receive a cupcake party",
+        "description": "Your student's entire class will receive a cupcake party",
+        "starting_bid": 20.00,
+    },
+    {
+        "name": "Donut Party #1",
+        "description": "Your student's entire class will receive a donut party",
+        "starting_bid": 20.00,
+    },
+    {
+        "name": "Donut Party #2",
+        "description": "Your student's entire class will receive a donut party",
         "starting_bid": 20.00,
     },
     {
         "name": "Donut Party #3",
-        "description": "Entire class will receive a donut party",
+        "description": "Your student's entire class will receive a donut party",
         "starting_bid": 20.00,
-    },
-    {
-        "name": "Pizza Party",
-        "description": "Entire class will enjoy pizza and games at Maize City Park",
-        "starting_bid": 20.00,
-    },
-    {
-        "name": "Sonic Drink Party",
-        "description": "Entire class will earn Sonic drinks delivered to them at school",
-        "starting_bid": 20.00, # VERIFY THIS STARTING PRICE
-    },
-    {
-        "name": "Lunch with Ms. Dow",
-        "description": "Student will go out to lunch with Ms. Dow!",
-        "starting_bid": 20.00, # VERIFY THIS STARTING PRICE
     },
 ]
 
